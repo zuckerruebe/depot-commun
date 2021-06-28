921f772c37b61147481bfac1c06d41038c564f89
Damian Birchler <damian.birchler@ost.ch>
2021-06-28 14:35:10 +0200

    Prototyping post'ing a purchase.

--- buying-frontend/src/Items.tsx
+++ buying-frontend/src/Items.tsx
@@ -1,18 +1,25 @@
 import React, { FunctionComponent, useState, useEffect } from 'react';
 import { RouterProps } from 'react-router-dom';
-import { Box, Paragraph } from 'grommet';
+import { Box, Button, Paragraph } from 'grommet';
+import { NumberInput } from 'grommet-controls'
 import axios from 'axios';
+import { useAuth } from './context/auth';
 
 type Item = {
   product_nr: string,
   tags: string,
   name: string,
   price: number,
-};
+}
+
+type ItemWithAmount = {
+  item: Item,
+  amount: number
+}
 
 const Items : FunctionComponent<RouterProps> = (props) => {
 
-  const [list, setList ] = useState<Item []>([]);
+  const [itemsWithAmount, setItemsWithAmount] = useState<ItemWithAmount[]>([]);
 
   useEffect(() => {
     // called when component has mounted
@@ -20,30 +27,55 @@ const Items : FunctionComponent<RouterProps> = (props) => {
     // load items list
     axios.get('/buying/items/available-items/')
     .then(response => {
-      setList(response.data);
+      const items: Item[] = response.data
+      const initialItemsWithAmount = items.map((i) => {return {item: i, amount: 0}})
+      setItemsWithAmount(initialItemsWithAmount)
     })
     .catch(error => {
       console.log('error on getting items: ' + error)
     })
   }, [])
 
-  return (
-    <Box direction="column" justify="start" overflow="scroll" fill="horizontal">
-    { list.map(itm => (
-      <Box direction="row" border={{"style":"solid"}} 
-          pad="medium" gap="medium" flex={false}>
-        <Paragraph>{ itm.product_nr }</Paragraph>
+  const auth = useAuth()
+
+  function createItemBox(itemWithAmount: ItemWithAmount): JSX.Element {
+    const item = itemWithAmount.item
+    return (
+      <Box direction="row" border={{ "style": "solid" }}
+        pad="medium" gap="medium" flex={false}>
+        <Paragraph>{item.product_nr}</Paragraph>
         <Box align="start" justify="center" fill="horizontal">
           <Paragraph>
-            { itm.name }
+            {item.name}
           </Paragraph>
         </Box>
+        <NumberInput textAlign="end"
+            value={itemWithAmount.amount.toString()}
+            onChange={(x: {target: HTMLInputElement | null}) => {itemWithAmount.amount = parseInt(x.target!.value); setItemsWithAmount([...itemsWithAmount])}}
+            min={0}
+            thousandsSeparatorSymbol="." />
         <Paragraph textAlign="end">
-          { itm.price }
+          {item.price}
         </Paragraph>
       </Box>
-    ))}
+    )
+  }
+
+  async function makePurchase() {
+    const itemsWithPositiveAmount = itemsWithAmount.filter(ia => ia.amount > 0);
+    const purchase = { items: itemsWithPositiveAmount.map(ia => {return {product_nr: ia.item.product_nr, quantity: ia.amount}}) }
+    axios.post(`/buying/users/${auth.userId}/purchases/`, purchase).catch(error =>
+      console.log('error posting purchase: ' + error)
+    )
+  }
+
+  return (
+    <Box direction="column" justify="start" overflow="scroll" fill="horizontal">
+    { itemsWithAmount.map(createItemBox)}
+    <Button onClick={makePurchase}>Make purchase</Button>
   </Box>
-)};
+  )
+}
 
 export default Items;
+
